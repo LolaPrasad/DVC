000c46573a1fd79dfcc23d10ba59f8434541aecb
Prasad Lola <prasadlola@gmail.com>
2023-11-30 17:06:52 +0530

    fourth commit

--- test.py
+++ test.py
@@ -3,8 +3,11 @@ import pandas as pd
 data = [
     {"name":"Prasad","age" : 23,"city":"Solapur" },
     {"name":"Alice","age" : 25,"city":"pune" },
-    {"name":"Tom","age" : 32,"city":"delhi" }
+    {"name":"Tom","age" : 32,"city":"delhi" },
+    {"name":"Ben","age" : 36,"city":"chennai" }
 ]
 
 df=pd.DataFrame(data)
+
+df.to_csv("data/data.csv",index=False)
 
