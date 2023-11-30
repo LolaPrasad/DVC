d325e81a23dfa335593a920ef5b7977ddff9dc9d
Prasad Lola <prasadlola@gmail.com>
2023-11-30 16:09:09 +0530

    second commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+data = [
+    {"name":"Prasad","age" : 23,"city":"Solapur" },
+    {"name":"Alice","age" : 25,"city":"pune" },
+    {"name":"Tom","age" : 32,"city":"delhi" }
+]
+
+df=pd.DataFrame(data)
+
